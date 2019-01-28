bdeaecdc5e1d40bfcf5f7fa36b6f9274c010cc62
Chris Leibl <leibl@google.com>
2019-01-28 10:49:24 -0500

    update function to take inputs from pubsub

--- gsuite-exporter-cloudfunction/main.py
+++ gsuite-exporter-cloudfunction/main.py
@@ -1,11 +1,25 @@
 import os
+import base64
+import json
 from gsuite_exporter.cli import sync_all
 
 def run (data, context):
+
+    # Decode Data From Pubsub Message
+    name = base64.b64decode(data['data']).decode('utf-8')
+
+    # Load Data in JSON
+    json_string = json.loads(name)
+    
+    # Parse JSON to Set Variables
+    project_id = json_string['PROJECT_ID']  
+    gsuite_admin_user = json_string['GSUITE_ADMIN_USER']
+
+    # Run Log Sync
     sync_all(
-            admin_user=os.environ['GSUITE_ADMIN_USER'],
+            admin_user=gsuite_admin_user,
             api='reports_v1',
             applications=['login', 'admin', 'drive', 'mobile', 'token'],
-            project_id=os.environ['PROJECT_ID'],
+            project_id=project_id,
             exporter_cls='stackdriver_exporter.StackdriverExporter'
         )
--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "google_pubsub_topic" "trigger-topic" {
 resource "null_resource" "cloud-scheduler" {
   provisioner "local-exec" {
     when = "create"
-    command = "gcloud beta scheduler jobs create pubsub gsuite-audit-log-scheduler --schedule=\"${var.cs_schedule}\" --topic=${google_pubsub_topic.trigger-topic.name} --message-body=run --project=${var.project_id}" 
+    command = "gcloud beta scheduler jobs create pubsub gsuite-audit-log-scheduler --schedule=\"${var.cs_schedule}\" --topic=${google_pubsub_topic.trigger-topic.name} --message-body='{\"PROJECT_ID\":\"${var.project_id}\",\"GSUITE_ADMIN_USER\":\"${var.gsuite_admin_user}\"}' --project=${var.project_id}" 
   }
 
     provisioner "local-exec" {
